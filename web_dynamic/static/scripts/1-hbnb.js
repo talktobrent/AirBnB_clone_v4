2ba04f22fed11224e71c11b096593490f5f81fdf
thenicopixie <391@holbertonschool.com>
2019-02-22 23:50:30 +0000

    move keys list into change function

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,15 +2,13 @@ $(document).ready(function () {
   let dict = {};
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
-      console.log($(this).attr('data-name') + ', ' + $(this).attr('data-id'));
       dict[$(this).attr('data-name')] = $(this).attr('data-id');
     } else {
-      delete dict.$(this).attr('data-id');
+      delete dict[$(this).attr('data-name')];
     }
-  });
-  $('DIV.amenities.h4').text(function () {
+  $('DIV.amenities>h4').text(function () {
     let keys = Object.keys(dict);
-    console.log(keys.join(', '));
     return keys.join(', ');
+  });
   });
 });
