10be63f7543cd42ba9545d077c8d3fb2feed0455
thenicopixie <391@holbertonschool.com>
2019-02-22 20:31:21 +0000

    add cache_id for render template

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-
+import uuid
 
 # flask setup
 app = Flask(__name__)
@@ -34,11 +34,12 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users)
+                           users=users,
+                           cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
     """
