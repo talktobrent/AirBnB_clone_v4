1df7c57de21a4663baaea4e97db924a476336a05
talktobrent <talktobrent@gmail.com>
2019-02-23 15:15:14 -0800

    Put HTML string into seperate function

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,3 +1,34 @@
+function template (place) {
+  return `
+<article>
+<div class="title">
+<h2>${place.name}</h2>
+<div class="price_by_night">
+${place.price_by_night}
+</div>
+</div>
+<div class="information">
+<div class="max_guest">
+<i class="fa fa-users fa-3x" aria-hidden="true"></i>
+<br />
+${place.max_guest} Guests
+</div>
+<div class="number_rooms">
+<i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+<br />
+${place.number_rooms} Bedrooms
+</div>
+<div class="number_bathrooms">
+<i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+<br />
+${place.number_bathrooms} Bathroom
+</div>
+</div>
+<div class="description">
+${place.description}
+</div>
+</article>`;
+}
 $(document).ready(function () {
   let dict = {};
   $('input:checkbox').css('margin-right', '10px');
@@ -30,34 +61,7 @@ $(document).ready(function () {
         return 0;
       });
       for (let place of data) {
-        $('section.places').append(`<article>
-	    		<div class="title">
-	      	<h2>${place.name}</h2>
-					<div class="price_by_night">
-					${place.price_by_night}
-	      	</div>
-	    		</div>
-	    		<div class="information">
-	      	<div class="max_guest">
-					<i class="fa fa-users fa-3x" aria-hidden="true"></i>
-					<br />
-					${place.max_guest} Guests
-	      	</div>
-	      	<div class="number_rooms">
-					<i class="fa fa-bed fa-3x" aria-hidden="true"></i>
-					<br />
-					${place.number_rooms} Bedrooms
-	      	</div>
-	      	<div class="number_bathrooms">
-					<i class="fa fa-bath fa-3x" aria-hidden="true"></i>
-					<br />
-					${place.number_bathrooms} Bathroom
-	      	</div>
-	    		</div>
-	    		<div class="description">
-	      	${place.description}
-	    		</div>
-	  			</article>`);
+        $('section.places').append(template(place));
       }
     }
   });
