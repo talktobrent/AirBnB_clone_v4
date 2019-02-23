d9862f784b633641c226e5350940fa0f69f4dab0
thenicopixie <391@holbertonschool.com>
2019-02-23 21:24:16 +0000

    create ajax dict for post request

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  let dict = {};
+  $('input:checkbox').css('margin-right', '10px');
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      dict[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete dict[$(this).attr('data-name')];
+    }
+    $('DIV.amenities>h4').text(function () {
+      let keys = Object.keys(dict);
+      return keys.join(', ');
+    });
+  });
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: JSON.stringify({}),
+    Content-Type: 'application/json',
+    success: function (data) {
+      
+    }
+  });
+});
